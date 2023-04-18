3ca3c4ce9676a2d739e9da691c6ec156c9dc77ec
Facundo Medica <facundomedica@gmail.com>
2023-04-18 20:42:27 -0300

    Revert "Revert "chore: update proto builder""
    This reverts commit da659cc5552eb65d22a6f17ef9d2c596cf576370.

--- module/contrib/local/protocgen.sh
+++ module/contrib/local/protocgen.sh
@@ -2,33 +2,18 @@
 
 set -eo pipefail
 
-if [ -z $GOPATH ]; then
-	echo "GOPATH not set!"
-	exit 1
-fi
-
-if [[ $PATH != *"$GOPATH/bin"* ]]; then
-	echo "GOPATH/bin must be added to PATH"
-	exit 1
-fi
-
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+cd proto
+proto_dirs=$(find -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  buf protoc \
-  -I "proto" \
-  -I "third_party/proto" \
-  --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
-
-  # # command to generate gRPC gateway (*.pb.gw.go in respective modules) files
-  buf protoc \
-  -I "proto" \
-  -I "third_party/proto" \
-  --grpc-gateway_out=logtostderr=true,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
-
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package $file &> /dev/null ; then
+      buf generate --template buf.gen.gogo.yaml $file
+    fi
+  done
 done
+
+cd ..
+
 
 # move proto files to the right places
 cp -r github.com/Gravity-Bridge/Gravity-Bridge/module/* ./
