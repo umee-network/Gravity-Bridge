443913c0267942a6ae529c35ff9b4711da29d88d
Facundo Medica <facundomedica@gmail.com>
2023-04-18 20:46:40 -0300

    rmeove changes

--- module/x/gravity/keeper/keeper_valset.go
+++ module/x/gravity/keeper/keeper_valset.go
@@ -337,7 +337,7 @@ func (k Keeper) GetCurrentValset(ctx sdk.Context) (types.Valset, error) {
 // normalizeValidatorPower scales rawPower with respect to totalValidatorPower to take a value between 0 and 2^32
 // Uses BigInt operations to avoid overflow errors
 // Example: rawPower = max (2^63 - 1), totalValidatorPower = 1 validator: (2^63 - 1)
-//   result: (2^63 - 1) * 2^32 / (2^63 - 1) = 2^32 = 4294967296 [this is the multiplier value below, our max output]
+//   result: (2^63 - 1) * 2^32 / (1000(2^63 - 1)) = 2^32 / 1000 = 4294967
 // Example: rawPower = max (2^63 - 1), totalValidatorPower = 1000 validators with the same power: 1000*(2^63 - 1)
 //	result: (2^63 - 1) * 2^32 / (1000(2^63 - 1)) = 2^32 / 1000 = 4294967
 func normalizeValidatorPower(rawPower uint64, totalValidatorPower sdk.Int) uint64 {
